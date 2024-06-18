030b8bcd04c822017d3b80676c34e5edc1f42beb
yw5123 <onestar5123@naver.com>
2024-06-18 17:57:15 +0900

    퍼즐게임 - 퍼즐 틀 세팅(JS)

--- 01.Basic/Yeowon/popup.js
+++ 01.Basic/Yeowon/popup.js
@@ -1,1 +1,33 @@
-console.log("This is a popup!")
+const game_board = document.querySelector('.board');
+const game_size = 3;
+
+let card_state = [];
+
+function game_init() {
+    for(let i = 1; i < game_size * game_size; i++) {
+        card_state.push(i);
+    }
+    card_state.push(null);
+
+    set_card();
+}
+
+function set_card() {
+    for(let i = 0; i < card_state.length; i++) {
+        const card = document.createElement('div');
+        if (card_state[i] == null) {
+            card.textContent = ""
+        } else {
+            card.textContent = card_state[i];
+        }
+        card.addEventListener('click', () => move_card(i));
+        game_board.appendChild(card);
+    }
+}
+
+function move_card(index) {
+    console.log('카드 클릭됨', index);
+}
+
+
+window.onload = game_init;

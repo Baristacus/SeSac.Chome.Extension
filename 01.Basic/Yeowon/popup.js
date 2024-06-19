315b232692b6d2aafe9a6ae1592a159ce6d3df64
yw5123 <onestar5123@naver.com>
2024-06-19 19:26:04 +0900

    퍼즐게임 - js 코드 기본 기능 완성

--- 01.Basic/Yeowon/popup.js
+++ 01.Basic/Yeowon/popup.js
@@ -1,18 +1,56 @@
 const game_board = document.querySelector('.board');
 const game_size = 3;
+const card_num = game_size * game_size;
 
 let card_state = [];
 
 function game_init() {
     card_state = [];
-    for (let i = 1; i < game_size * game_size; i++) {
+
+    document.querySelector('.board').style.gridTemplateColumns = `repeat(${game_size}, 1fr)`;
+    
+    for (let i = 1; i < card_num; i++) {
         card_state.push(i);
     }
-    document.querySelector('.board').style.gridTemplateColumns = `repeat(${game_size}, 1fr)`;
-
     card_state.push(null);
 
+    shuffle_card();
     set_card();
+}
+
+function shuffle_card() {
+    for (let i = card_state.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [card_state[i], card_state[j]] = [card_state[j], card_state[i]];
+    }
+    if (!is_game_valid()) {
+        shuffle_card();
+    }
+}
+
+function is_game_valid() {
+    const empty_index = card_state.indexOf(null);
+    const from_bottom = game_size - Math.floor(empty_index / game_size);
+
+    let inversion = 0;
+    for (let i = 0; i < card_num - 1; i++) {
+        for (let j = i + 1; j < card_num; j++) {
+            if (card_state[i] && card_state[j] && card_state[i] > card_state[j]) {
+                inversion++;
+            }
+        }
+    }
+
+    if (game_size % 2 == 1 && inversion % 2 == 0) {
+        return true;
+    } else if (game_size % 2 == 0) {
+        if (from_bottom % 2 == 0 && inversion % 2 == 1) {
+            return true;
+        } else if (from_bottom % 2 == 1 && inversion % 2 == 0) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function set_card() {
@@ -29,14 +67,28 @@ function set_card() {
     }
 }
 
+function is_game_solved() {
+    for (let i = 0; i < card_state.length - 1; i++) {
+        if (card_state[i] != i + 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function move_card(index) {
     const empty_index = card_state.indexOf(null);
-    console.log('카드 클릭됨', empty_index);
 
     if (is_move_valid(index, empty_index)) {
         card_state[empty_index] = card_state[index];
         card_state[index] = null;
         set_card();
+
+        if (is_game_solved()) {
+            setTimeout(() => {
+                alert('게임 성공!');
+            }, 0);       
+        }
     }
 }
 
